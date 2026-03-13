ac73f8fc50ec10538ce37491463e8e6989e2f42b
Kimberly Brown <kjbrown.home@gmail.com>
2026-03-13 17:13:55 -0600

    Flesh out upgrade manager

--- autoloads/upgrade_manager/upgrade_manager.gd
+++ autoloads/upgrade_manager/upgrade_manager.gd
@@ -4,16 +4,17 @@ class UpgradeState:
 	var level: int = 0
 	var cost: int = 0
 	var delta: int = 0
+	var max_level: int = 0  ## 0 = uncapped; mutable for cap raises
 
 signal upgrade_purchased(upgrade_id, board_type, new_level)
 
-## Populate this array in the Inspector with .tres UpgradeData resources.
-@export var upgrades: Array[UpgradeData] = []
+## Populate this array in the Inspector with .tres BaseUpgradeData resources.
+@export var upgrades: Array[BaseUpgradeData] = []
 
 ## Per-board, per-upgrade runtime state.
 var _state: Dictionary = {}  # BoardType -> upgrade_id -> UpgradeState
 
-## Quick lookup: upgrade_id -> UpgradeData
+## Quick lookup: upgrade_id -> BaseUpgradeData
 var _upgrade_map: Dictionary = {}
 
 
@@ -29,6 +30,7 @@ func _ready() -> void:
 			var s := UpgradeState.new()
 			s.cost = data.base_cost
 			s.delta = data.cost_delta
+			s.max_level = data.max_level
 			_state[board_type][data.id] = s
 
 	# Debug: print initial state
@@ -41,7 +43,7 @@ func _ready() -> void:
 			])
 
 
-func get_upgrade(id: String) -> UpgradeData:
+func get_upgrade(id: String) -> BaseUpgradeData:
 	return _upgrade_map.get(id)
 
 
@@ -53,20 +55,23 @@ func get_cost(board_type: Enums.BoardType, upgrade_id: String) -> int:
 	return _state[board_type][upgrade_id].cost
 
 
-func get_max_level(upgrade_id: String) -> int:
-	return _upgrade_map[upgrade_id].max_level
+func get_max_level(board_type: Enums.BoardType, upgrade_id: String) -> int:
+	return _state[board_type][upgrade_id].max_level
+
+
+func get_state(board_type: Enums.BoardType, upgrade_id: String) -> UpgradeState:
+	return _state[board_type][upgrade_id]
 
 
 func can_buy(board_type: Enums.BoardType, upgrade_id: String) -> bool:
-	var data: UpgradeData = _upgrade_map[upgrade_id]
-	var level: int = _state[board_type][upgrade_id].level
+	var state: UpgradeState = _state[board_type][upgrade_id]
 
 	# max_level of 0 means uncapped
-	if data.max_level > 0 and level >= data.max_level:
+	if state.max_level > 0 and state.level >= state.max_level:
 		return false
 
-	# TODO: check CurrencyManager.can_afford() once it exists
-	return true
+	var currency := _currency_for_board(board_type)
+	return CurrencyManager.can_afford(currency, state.cost)
 
 
 func buy(board_type: Enums.BoardType, upgrade_id: String) -> bool:
@@ -74,28 +79,38 @@ func buy(board_type: Enums.BoardType, upgrade_id: String) -> bool:
 		return false
 
 	var state: UpgradeState = _state[board_type][upgrade_id]
-	var data: UpgradeData = _upgrade_map[upgrade_id]
 
-	# TODO: CurrencyManager.spend(currency, state.cost)
+	var currency := _currency_for_board(board_type)
+	CurrencyManager.spend(currency, state.cost)
 
 	state.level += 1
 	_advance_cost(board_type, upgrade_id)
-
-	# TODO: apply effect based on upgrade_id
 
 	upgrade_purchased.emit(upgrade_id, board_type, state.level)
 	return true
 
 
+func _currency_for_board(board_type: Enums.BoardType) -> Enums.CurrencyType:
+	match board_type:
+		Enums.BoardType.GOLD:
+			return Enums.CurrencyType.GOLD_COIN
+		Enums.BoardType.ORANGE:
+			return Enums.CurrencyType.ORANGE_COIN
+		Enums.BoardType.RED:
+			return Enums.CurrencyType.RED_COIN
+		_:
+			return Enums.CurrencyType.GOLD_COIN
+
+
 func _advance_cost(board_type: Enums.BoardType, upgrade_id: String) -> void:
 	var upgrade_state: UpgradeState = _state[board_type][upgrade_id]
-	var data: UpgradeData = _upgrade_map[upgrade_id]
+	var data: BaseUpgradeData = _upgrade_map[upgrade_id]
 
 	match data.cost_type:
-		UpgradeData.CostType.ADDITIVE:
+		BaseUpgradeData.CostType.ADDITIVE:
 			upgrade_state.cost += upgrade_state.delta
-		UpgradeData.CostType.ADDITIVE_ESCALATING:
+		BaseUpgradeData.CostType.ADDITIVE_ESCALATING:
 			upgrade_state.cost += upgrade_state.delta
 			upgrade_state.delta += data.delta_escalation
-		UpgradeData.CostType.MULTIPLICATIVE:
+		BaseUpgradeData.CostType.MULTIPLICATIVE:
 			upgrade_state.cost = int(upgrade_state.cost * data.cost_multiplier)
--- scripts/base_upgrade_data.gd
+++ scripts/base_upgrade_data.gd
@@ -1,4 +1,4 @@
-class_name UpgradeData
+class_name BaseUpgradeData
 extends Resource
 
 @export var id: String
