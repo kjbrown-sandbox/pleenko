e74b4ae4006d90fa3101893bd19ce5a2e2aa84e3
Kimberly Brown <kjbrown.home@gmail.com>
2026-03-18 17:58:33 -0600

    Add stubs for challenges menu

--- entities/main_menu/main_menu.gd
+++ entities/main_menu/main_menu.gd
@@ -1,11 +1,17 @@
 extends Node3D
 
 const MainScene := preload("res://entities/main/main.tscn")
+const ChallengesMenuScene := preload("res://entities/challenges_menu/challenges_menu.tscn")
 
 @onready var play_button: Button = $CanvasLayer/HBoxContainer/PlayButton
+@onready var challenges_button: Button = $CanvasLayer/HBoxContainer/ChallengesButton
 
 func _ready() -> void:
    play_button.pressed.connect(_on_play_pressed)
+   challenges_button.pressed.connect(_on_challenges_pressed)
 
 func _on_play_pressed() -> void:
    SceneManager.set_new_scene(MainScene)
+
+func _on_challenges_pressed() -> void:
+   SceneManager.set_new_scene(ChallengesMenuScene)
