26fc6c005da00b170049b046b461032cd426cf0e
Kimberly Brown <kjbrown.home@gmail.com>
2026-03-25 21:33:20 -0600

    Add a fetching awesome settings icon

--- entities/icon/icon.gd
+++ entities/icon/icon.gd
@@ -1,9 +1,14 @@
 extends TextureButton
 
-@export var icon_texture: Texture2D
-@export var color_source: VisualTheme.Palette = VisualTheme.Palette.BG_5   
+const IconTintShader := preload("res://entities/icon/icon_tint.gdshader")
 
+@export var icon_texture: Texture2D
+@export var color_source: VisualTheme.Palette = VisualTheme.Palette.BG_5
+
+# Icon from: <a href="https://www.flaticon.com/free-icons/configure" title="configure icons">Configure icons created by logisstudio - Flaticon</a>
 func _ready() -> void:
 	texture_normal = icon_texture
-	modulate = ThemeProvider.theme._resolve(color_source)     
-	# modulate = ThemeProvider.theme.background_color
+	var mat := ShaderMaterial.new()
+	mat.shader = IconTintShader
+	mat.set_shader_parameter("tint_color", ThemeProvider.theme._resolve(color_source))
+	material = mat
